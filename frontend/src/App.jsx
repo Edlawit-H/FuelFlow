94b990c8651884fdbe066d210a86b32f21e58205
RahmetAbdelah <rahmetmera5@gmail.com>
2026-04-27 14:17:00 +0300

    the initial landing page of fuelflow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,18 @@ import { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import DriverHome from './pages/DriverHome';
 import './App.css';
+import FuelFlowLanding from './pages/FuelFlowLanding';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('home');
+ // const [currentPage, setCurrentPage] = useState('home');
 
   return (
-    <div className="flex bg-slate-50 min-h-screen">
-      <Sidebar activePage={currentPage} setPage={setCurrentPage} />
-      
-      <main className="flex-1">
-        {currentPage === 'home' && <DriverHome />}
-        {currentPage === 'stations' && <div className="p-20 text-center text-slate-400">Station Detail View Loaded Here</div>}
-        {/* We will add more pages here as you upload them */}
-      </main>
-    </div>
+    <>
+    <FuelFlowLanding/>
+
+
+    
+    </>
   );
 }
 
